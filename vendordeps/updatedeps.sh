ce815b15633a9e3e2fc446142f9751393b603fac
Irishumanoid <95321751+Irishumanoid@users.noreply.github.com>
2024-01-09 22:25:12 -0800

    update vendordeps automatically

---
+++ vendordeps/updatedeps.sh
@@ -0,0 +1,7 @@
+rm -r ~/vendordeps/*
+wget https://maven.ctr-electronics.com/release/com/ctre/phoenix6/latest/Phoenix6-frc2024-latest.json
+wget https://raw.githubusercontent.com/wpilibsuite/allwpilib/main/wpilibNewCommands/WPILibNewCommands.json
+wget https://software-metadata.revrobotics.com/REVLib-2024.json
+wget https://maven.photonvision.org/repository/internal/org/photonvision/photonlib-json/1.0/photonlib-json-1.0.json
+wget https://3015rangerrobotics.github.io/pathplannerlib/PathplannerLib.json
+wget https://sleipnirgroup.github.io/ChoreoLib/dep/ChoreoLib.json
