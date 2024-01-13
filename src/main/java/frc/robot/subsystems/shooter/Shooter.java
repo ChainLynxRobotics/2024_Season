a6bb9d7e9a495fde2d0504d3f0fe6348e906c656
awfulbananas <Joseph.Fromel@gmail.com>
2024-01-13 12:15:04 -0800

    added subsystem stubs

---
+++ src/main/java/frc/robot/subsystems/shooter/Shooter.java
@@ -0,0 +1,20 @@
+package frc.robot.subsystems.shooter;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Shooter extends SubsystemBase {
+  /** Creates a new ExampleSubsystem. */
+  public Shooter() {
+    
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    // This method will be called once per scheduler run during simulation
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/vision/Vision.java
@@ -0,0 +1,20 @@
+package frc.robot.subsystems.vision;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Vision extends SubsystemBase {
+  /** Creates a new ExampleSubsystem. */
+  public Vision() {
+    
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    // This method will be called once per scheduler run during simulation
+  }
+}
