bbad13ed3de80ff1c001d3be24518ef8ac448f2b
awfulbananas <Joseph.Fromel@gmail.com>
2024-01-13 12:16:24 -0800

    renamed DriveSubsystem.java to Drivetrain.java

--- src/main/java/frc/robot/subsystems/drive/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/drive/Drivetrain.java
@@ -2,9 +2,9 @@ package frc.robot.subsystems.drive;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class DriveSubsystem extends SubsystemBase {
+public class Drivetrain extends SubsystemBase {
   /** Creates a new ExampleSubsystem. */
-  public DriveSubsystem() {
+  public Drivetrain() {
     
   }
 
