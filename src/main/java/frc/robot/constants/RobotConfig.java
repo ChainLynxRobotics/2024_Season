6e547efbf3d40f3321acafe2b2f72ebddb2e4685
awfulbananas <Joseph.Fromel@gmail.com>
2024-01-13 13:10:53 -0800

    added drive constants and config

---
+++ src/main/java/frc/robot/constants/RobotConfig.java
@@ -0,0 +1,15 @@
+package frc.robot.constants;
+//variable things like default speeds
+public class RobotConfig {
+
+    public static class DriveConfig {
+
+        public static final double kFrontLeftChassisAngularOffset = 0.0;
+        public static final double kFrontRightChassisAngularOffset = 0.0;
+        public static final double kBackLeftChassisAngularOffset = 0.0;
+        public static final double kBackRightChassisAngularOffset = 0.0;
+    
+        
+    }
+    
+}
