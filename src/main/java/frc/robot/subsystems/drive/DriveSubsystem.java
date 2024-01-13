4c348df1a33e40ba36e5278996812c63b4856daa
awfulbananas <Joseph.Fromel@gmail.com>
2024-01-13 09:59:04 -0800

    added DriveSubsystem.java

---
+++ src/main/java/frc/robot/subsystems/drive/DriveSubsystem.java
@@ -0,0 +1,18 @@
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class DriveSubsystem extends SubsystemBase {
+  /** Creates a new ExampleSubsystem. */
+  public Drivetrain() {
+    
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    // This method will be called once per scheduler run during simulation
+  }
+}
