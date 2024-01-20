3e69077a2caa90b9ca594ec0b6b085a7c8fc2c40
EmDash00 <emberchow00@gmail.com>
2024-01-20 13:38:44 -0800

    accept rebase conflicts from drivetrain constants p2

--- src/main/java/frc/robot/constants/RobotConfig.java
+++ src/main/java/frc/robot/constants/RobotConfig.java
@@ -9,6 +9,20 @@ public class RobotConfig {
         public static final double kBackLeftChassisAngularOffset = 0.0;
         public static final double kBackRightChassisAngularOffset = 0.0;
 
+        //4.45 m/s max speed
+        public static final double kMaxSpeedBase = 4.8;
+        public static final double kMaxSpeedScaleFactor = 0.9;
+        public static final double kMaxSpeedMetersPerSecond =
+            kMaxSpeedBase * kMaxSpeedScaleFactor;
+
+        public static final double kMaxAngularSpeedBase = Math.PI;
+        public static final double kMaxAngularSpeedScaleFactor = 0.7;
+        public static final double kMaxAngularSpeed =
+            kMaxAngularSpeedBase * kMaxAngularSpeedScaleFactor; // radians per second
+
+        //scaling factor for the alternative turning mode
+        public static final int altTurnSmoothing = 20;
+
 
     }
 
