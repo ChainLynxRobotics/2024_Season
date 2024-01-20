8145f5e29b392e47e0c03b8746b12f07b92f1c48
EmDash00 <emberchow00@gmail.com>
2024-01-20 14:02:40 -0800

    applied spotless to drivetrain files

--- src/main/java/frc/robot/constants/RobotConfig.java
+++ src/main/java/frc/robot/constants/RobotConfig.java
@@ -1,19 +1,19 @@
 package frc.robot.constants;
-//variable things like default speeds
+
+// variable things like default speeds
 public class RobotConfig {
 
-    public static class DriveConfig {
+  public static class DriveConfig {
 
-        //4.45 m/s max speed
-        public static final double kMaxSpeedBase = 4.8;
-        public static final double kMaxSpeedScaleFactor = 0.9;
-        public static final double kMaxSpeedMetersPerSecond =
-            kMaxSpeedBase * kMaxSpeedScaleFactor;
+    // 4.45 m/s max speed
+    public static final double kMaxSpeedBase = 4.8;
+    public static final double kMaxSpeedScaleFactor = 0.9;
+    public static final double kMaxSpeedMetersPerSecond = kMaxSpeedBase * kMaxSpeedScaleFactor;
 
-        public static final double kMaxAngularSpeedBase = Math.PI;
-        public static final double kMaxAngularSpeedScaleFactor = 0.7;
-        public static final double kMaxAngularSpeed =
-            kMaxAngularSpeedBase * kMaxAngularSpeedScaleFactor; // radians per second
+    public static final double kMaxAngularSpeedBase = Math.PI;
+    public static final double kMaxAngularSpeedScaleFactor = 0.7;
+    public static final double kMaxAngularSpeed =
+        kMaxAngularSpeedBase * kMaxAngularSpeedScaleFactor; // radians per second
 
         public static final double kFrontLeftChassisAngularOffset = 0.0;
         public static final double kFrontRightChassisAngularOffset = 0.0;
