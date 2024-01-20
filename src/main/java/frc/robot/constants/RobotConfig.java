1f41674a165595303612ce7630e114a2e2e451ca
EmDash00 <emberchow00@gmail.com>
2024-01-20 13:36:43 -0800

    accept rebase conflicts from drivetrain constants

--- src/main/java/frc/robot/constants/RobotConfig.java
+++ src/main/java/frc/robot/constants/RobotConfig.java
@@ -1,7 +1,15 @@
 package frc.robot.constants;
+//variable things like default speeds
+public class RobotConfig {
 
-/**
- * Software config settings (e.g. max speed, PID values). For hardware constants @see
- * RobotConstants"
- */
-public class RobotConfig {}
+    public static class DriveConfig {
+
+        public static final double kFrontLeftChassisAngularOffset = 0.0;
+        public static final double kFrontRightChassisAngularOffset = 0.0;
+        public static final double kBackLeftChassisAngularOffset = 0.0;
+        public static final double kBackRightChassisAngularOffset = 0.0;
+
+
+    }
+
+}
