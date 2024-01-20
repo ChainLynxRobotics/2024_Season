14bb1d80ed140a823c469806f11da757e3452b29
EmDash00 <emberchow00@gmail.com>
2024-01-20 13:59:21 -0800

    fixed formating and removed extranious boolean

--- src/main/java/frc/robot/commands/BasicDriveCommand.java
+++ src/main/java/frc/robot/commands/BasicDriveCommand.java
@@ -8,52 +8,48 @@ import frc.robot.constants.RobotConstants.DriveConstants;
 import frc.robot.subsystems.drive.Drivetrain;
 
 public class BasicDriveCommand extends Command {
-    private Drivetrain drive;
-    private XboxController controller;
-    private double multiplier;
+    private Drivetrain m_drive;
+    private XboxController m_controller;
+    private double m_multiplier;
 
     public BasicDriveCommand(Drivetrain drive, XboxController controller) {
-        this.drive = drive;
-        this.controller = controller;
+        this.m_drive = drive;
+        this.m_controller = controller;
 
-        multiplier = 1;
+        this.m_multiplier = 1;
 
-        addRequirements(drive);
+        addRequirements(this.m_drive);
     }
 
     @Override
     public void execute() {
-        boolean slowMode = false;
-        if (controller.getLeftTriggerAxis() != 0) {
-            multiplier = 2;
-            slowMode = false;
-        } else if (controller.getRightTriggerAxis() != 0) {
-            multiplier = 0.5;
-            slowMode = true;
+        if (this.m_controller.getLeftTriggerAxis() != 0) {
+            this.m_multiplier = 2;
+            SmartDashboard.putBoolean("slow mode", false);
+        } else if (this.m_controller.getRightTriggerAxis() != 0) {
+            this.m_multiplier = 0.5;
+            SmartDashboard.putBoolean("slow mode", true);
         }
 
-        SmartDashboard.putBoolean("slow mode", slowMode);
-
-
-        drive.drive(
+        m_drive.drive(
             MathUtil.applyDeadband(
-                -multiplier * controller.getLeftY(),
+                -this.m_multiplier * this.m_controller.getLeftY(),
                 DriveConstants.kDriveDeadband
             ),
             MathUtil.applyDeadband(
-                -multiplier * controller.getLeftX(),
+                -this.m_multiplier * this.m_controller.getLeftX(),
                 DriveConstants.kDriveDeadband
             ),
             MathUtil.applyDeadband(
-                -multiplier * controller.getRightX(),
+                -this.m_multiplier * this.m_controller.getRightX(),
                 DriveConstants.kDriveDeadband
             ),
             MathUtil.applyDeadband(
-                -multiplier * controller.getRightY(),
+                -this.m_multiplier * this.m_controller.getRightY(),
                 DriveConstants.kDriveDeadband
             ),
-            controller.getRightBumper(),
-            controller.getAButton()
+            this.m_controller.getRightBumper(),
+            this.m_controller.getAButton()
         );
     }
 }
