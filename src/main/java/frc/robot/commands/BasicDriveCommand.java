9a6a0cb8c15ba459fa685d0eb760328fd2eba172
EmDash00 <emberchow00@gmail.com>
2024-01-27 13:28:04 -0800

    prev, but I actually staged the changes

--- src/main/java/frc/robot/commands/BasicDriveCommand.java
+++ src/main/java/frc/robot/commands/BasicDriveCommand.java
@@ -33,9 +33,10 @@ public class BasicDriveCommand extends Command {
     }
 
     m_drive.drive(
-        new Vector(
+        (new Vector(
             MathUtil.applyDeadband(-m_controller.getLeftY(), OIConstants.kDriveDeadband),
-            MathUtil.applyDeadband(-m_controller.getLeftX(), OIConstants.kDriveDeadband)),
+            MathUtil.applyDeadband(-m_controller.getLeftX(), OIConstants.kDriveDeadband)))
+            .mult(m_multiplier),
         new Vector(
             MathUtil.applyDeadband(-m_controller.getRightX(), OIConstants.kDriveDeadband),
             MathUtil.applyDeadband(-m_controller.getRightY(), OIConstants.kDriveDeadband)),
