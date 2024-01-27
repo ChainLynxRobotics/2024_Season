5c64787c019cdf8a0908c60ba6132b63fea9c018
awfulbananas <Joseph.Fromel@gmail.com>
2024-01-27 14:03:13 -0800

    made pid consts not nan

--- src/main/java/frc/robot/constants/RobotConstants.java
+++ src/main/java/frc/robot/constants/RobotConstants.java
@@ -93,17 +93,17 @@ public final class RobotConstants {
       public static final double kTurningEncoderPositionPIDMaxInput =
           kTurningEncoderPositionFactor; // radians
 
-      public static final double kDrivingP = Double.NaN;
-      public static final double kDrivingI = Double.NaN;
-      public static final double kDrivingD = Double.NaN;
+      public static final double kDrivingP =0.04;
+      public static final double kDrivingI = 0;
+      public static final double kDrivingD = 0;
       public static final double kDrivingFF = 1 / kDriveWheelFreeSpeedRps;
       public static final double kDrivingMinOutput = -1;
       public static final double kDrivingMaxOutput = 1;
 
-      public static final double kTurningP = Double.NaN;
-      public static final double kTurningI = Double.NaN;
-      public static final double kTurningD = Double.NaN;
-      public static final double kTurningFF = Double.NaN;
+      public static final double kTurningP = 1;
+      public static final double kTurningI = 0;
+      public static final double kTurningD = 0;
+      public static final double kTurningFF = 0;
       public static final double kTurningMinOutput = -1;
       public static final double kTurningMaxOutput = 1;
 
