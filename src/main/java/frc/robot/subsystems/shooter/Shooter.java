b22694243adee5292105d4b4e86c90c216b7cb6c
Robert Sun <145725240+WashEyesWithTide@users.noreply.github.com>
2024-01-26 16:34:08 -0800

    Updated shooter specs

--- src/main/java/frc/robot/subsystems/shooter/Shooter.java
+++ src/main/java/frc/robot/subsystems/shooter/Shooter.java
@@ -1,10 +1,16 @@
 package frc.robot.subsystems.shooter;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-/* two sets of rollers of the same size spin same direction,
-*  index of seconday rollers make note go shooter, controller for flywheels,
-*  difference of speed in flywheels determines angle and speed of note.
-*/
+/**
+ * two sets of rollers of the same size spin same direction,
+ * index of seconday rollers make note go shooter, controller for flywheels,
+ * difference of speed in flywheels determines angle and speed of note.
+ * PID controller per motor to account for different dynamics (no follower)
+ * periodic log velocity setpoints
+ * pid only in subsystems
+ * set net velocity, angle diff fywheel speed)
+ *
+**/
 public class Shooter extends SubsystemBase {
   /** Creates a new ExampleSubsystem. */
   public Shooter() {}
