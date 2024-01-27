c8addcca3c6306a6015c9756018cd8d6c12af826
Robert Sun <145725240+WashEyesWithTide@users.noreply.github.com>
2024-01-27 11:43:36 -0800

    Created ids for roller and angle control motors

--- src/main/java/frc/robot/constants/RobotConstants.java
+++ src/main/java/frc/robot/constants/RobotConstants.java
@@ -4,4 +4,11 @@ package frc.robot.constants;
  * Software/hardware constants (e.g. CAN IDs, gear ratios, field measurements, etc.). For software
  * configs @see RobotConfig
  */
-public final class RobotConstants {}
+public final class RobotConstants {
+  public final class ShooterConstants {
+    public static final int kRollerMotorLeftId = -1;
+    public static final int kRollerMotorRightId = -1;
+    public static final int kAngleMotorLeaderId = -1;
+    public static final int kAngleMotorFollowerId = -1;
+  }
+}
