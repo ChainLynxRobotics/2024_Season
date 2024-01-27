b4cee01bd752ce96a1561c9a546fa80c7940ca9e
ChainedGCH <failureeverywhere@gmail.com>
2024-01-26 17:24:35 -0800

    Removed simulation periodic

--- src/main/java/frc/robot/subsystems/intake/Intake.java
+++ src/main/java/frc/robot/subsystems/intake/Intake.java
@@ -63,10 +63,7 @@ public class Intake extends SubsystemBase {
     // This method will be called once per scheduler run
   }
 
-  @Override
-  public void simulationPeriodic() {
-    // This method will be called once per scheduler run during simulation
-  }
+
 
   /**
    * Runs the intake
