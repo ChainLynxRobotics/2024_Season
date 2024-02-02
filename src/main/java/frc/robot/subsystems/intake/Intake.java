09818ef92115f85a5d7b6d794decc4bd1c4513ba
AndrewS-123 <shanandrew121@gmail.com>
2024-02-01 18:53:10 -0800

    Added velocity logging on smart dashboard

--- src/main/java/frc/robot/subsystems/intake/Intake.java
+++ src/main/java/frc/robot/subsystems/intake/Intake.java
@@ -4,6 +4,7 @@ import com.revrobotics.CANSparkLowLevel.MotorType;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
 import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.constants.RobotConfig;
 import frc.robot.constants.RobotConstants;
@@ -29,6 +30,7 @@ public class Intake extends SubsystemBase {
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    SmartDashboard.putNumber("intake/roller velocity", m_rollerEncoder.getVelocity());
   }
 
   /**
