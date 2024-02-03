2d7520c1720837d43b0702134e1d46cf9049de60
ChainedGCH <failureeverywhere@gmail.com>
2024-02-02 16:27:05 -0800

    Removed encoders

--- src/main/java/frc/robot/subsystems/intake/Intake.java
+++ src/main/java/frc/robot/subsystems/intake/Intake.java
@@ -2,27 +2,20 @@ package frc.robot.subsystems.intake;
 
 import com.revrobotics.CANSparkLowLevel.MotorType;
 import com.revrobotics.CANSparkMax;
-import com.revrobotics.RelativeEncoder;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.constants.RobotConstants.IntakeConstants;
 
 public class Intake extends SubsystemBase {
   private final CANSparkMax m_rollerMotor; // right motor (Rollers)
 
-  private final RelativeEncoder m_rollerEncoder; // Roller Relative Encoder
-
   // Constructs intake and initializes motor, PID, encoder objects, sensor
   public Intake() {
     m_rollerMotor = new CANSparkMax(IntakeConstants.kMotorID, MotorType.kBrushless);
-
-    m_rollerEncoder = m_rollerMotor.getEncoder();
   }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
-    SmartDashboard.putNumber("intake/roller velocity", m_rollerEncoder.getVelocity());
   }
 
   /**
@@ -34,9 +27,7 @@ public class Intake extends SubsystemBase {
     m_rollerMotor.set(motorOutput);
   }
 
-  /**
-   * Stops the roller motor
-   */
+  /** Stops the roller motor */
   public void stop() {
     m_rollerMotor.stopMotor();
   }
