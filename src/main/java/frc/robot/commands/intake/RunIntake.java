d57734d4e92254693730a35a6f15b3d0727004cf
ChainedGCH <failureeverywhere@gmail.com>
2024-02-06 17:10:25 -0800

    Moved set intake speed to initialize from execute

--- src/main/java/frc/robot/commands/intake/RunIntake.java
+++ src/main/java/frc/robot/commands/intake/RunIntake.java
@@ -21,13 +21,13 @@ public class RunIntake extends Command {
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    m_intake.run(RobotConfig.IntakeConfig.kDefaultSpeed);
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {
-    m_intake.run(RobotConfig.IntakeConfig.kDefaultSpeed);
-  }
+  public void execute() {}
 
   // Called once the command ends or is interrupted.
   @Override
