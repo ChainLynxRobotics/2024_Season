59449785761927c2c46403e05a5214eeb36601f4
Robert Sun <145725240+WashEyesWithTide@users.noreply.github.com>
2024-02-08 19:25:35 -0800

    Create ShootSpeaker.java

---
+++ src/main/java/frc/robot/commands/shooter/ShootSpeaker.java
@@ -0,0 +1,32 @@
+package frc.robot.commands.shooter;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.shooter.Shooter;
+
+public class ShootSpeaker extends Command {
+  private final Shooter m_shooter;
+
+  public ShootSpeaker(Shooter shooter) {
+    m_shooter = shooter;
+
+    addRequirements(m_shooter);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {}
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
