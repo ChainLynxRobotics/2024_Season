98a227744dcc3b160a62c274d74df1c02761c3b5
Carter Mackey <carter.mackey@icloud.com>
2024-02-08 18:19:42 -0800

    implented autobuilder

--- src/main/java/frc/robot/subsystems/drive/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/drive/Drivetrain.java
@@ -1,14 +1,21 @@
 package frc.robot.subsystems.drive;
-
 import com.ctre.phoenix6.hardware.Pigeon2;
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.util.HolonomicPathFollowerConfig;
+import com.pathplanner.lib.util.PIDConstants;
+import com.pathplanner.lib.util.ReplanningConfig;
 import edu.wpi.first.math.filter.SlewRateLimiter;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics.SwerveDriveWheelStates;
 import edu.wpi.first.units.*;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.PowerDistribution;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -206,14 +213,30 @@ public class Drivetrain extends SubsystemBase {
     m_turnDirRadians = rot;
     move(spdVec, rot);
   }
+  public ChassisSpeeds getRobotRelativeSpeeds() {
+    return DriveConstants.kDriveKinematics.toChassisSpeeds(
+      new SwerveModuleState[] {
+        m_frontLeft.getState(),
+        m_frontRight.getState(),
+        m_rearLeft.getState(),
+        m_rearRight.getState()
+      }
+    );
 
+    // CCW rotation out of chassis frame
+ //   var rotated = new Translation2d(vxMetersPerSecond, vyMetersPerSecond).rotateBy(robotAngle);
+ //   return new ChassisSpeeds(rotated.getX(), rotated.getY(), omegaRadiansPerSecond);
+  }
   private double altTurnSmooth(double stickAng) {
     return Math.tanh(
             ((getGyroAngle().in(Units.Radians) + stickAng + Math.PI) % (2 * Math.PI) - Math.PI)
                 / DriveConfig.altTurnSmoothing)
         * DriveConfig.kMaxAngularSpeed;
   }
-
+  private Pose2d getPose() {
+    Pose2d pose = m_odometry.getPoseMeters();
+    return(pose);
+  }
   /**
    * moves the drivetrain using the given values
    *
@@ -316,6 +339,13 @@ public class Drivetrain extends SubsystemBase {
 
     return (new Vector(m_currentTranslationMag, 0)).rot(m_currentTranslationDirRadians);
   }
+  private boolean allianceCheck() {
+    var alliance = DriverStation.getAlliance();
+    if(alliance.isPresent()) {
+      return alliance.get() == DriverStation.Alliance.Red;
+    }
+    return(false);
+  }
 
   /** Zeroes the heading of the robot. */
   public void zeroHeading() {
@@ -327,5 +357,27 @@ public class Drivetrain extends SubsystemBase {
   @Override
   public void simulationPeriodic() {
     // This method will be called once per scheduler run during simulation
+  AutoBuilder.configureHolonomic(
+    this::getPose,
+    this::resetOdometry,
+    this::getRobotRelativeSpeeds,
+    this::altDrive,
+    new HolonomicPathFollowerConfig(
+      new PIDConstants(
+        DriveConstants.SwerveModuleConstants.kDrivingP,
+        DriveConstants.SwerveModuleConstants.kDrivingI,
+        DriveConstants.SwerveModuleConstants.kDrivingD
+      ),
+      new PIDConstants(
+        DriveConstants.SwerveModuleConstants.kTurningP,
+        DriveConstants.SwerveModuleConstants.kTurningI,
+        DriveConstants.SwerveModuleConstants.kTurningD
+      ),
+      DriveConfig.kMaxSpeedMetersPerSecond,
+      DriveConstants.kTrackWidth / 2,
+      new ReplanningConfig()),
+      this::allianceCheck,
+      this
+      );
   }
 }
