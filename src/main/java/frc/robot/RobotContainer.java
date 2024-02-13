6a5d4bf2f333d63dc29e6095179573a13a23780e
awfulbananas <Joseph.Fromel@gmail.com>
2024-02-13 15:51:02 -0800

    cleaned up previous RobotContainer changes

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -44,14 +44,7 @@ public class RobotContainer {
         // Turning is controlled by the X axis of the right stick.
         new RunCommand(
             () -> {
-                    leftInputVec.setX(MathUtil.applyDeadband(
-                                  -m_driverController.getLeftY(), OIConstants.kDriveDeadband));
-                    leftInputVec.setY(MathUtil.applyDeadband(
-                                  -m_driverController.getLeftX(), OIConstants.kDriveDeadband));
-                    rightInputVec.setX(MathUtil.applyDeadband(
-                                  -m_driverController.getRightX(), OIConstants.kDriveDeadband));
-                    rightInputVec.setY(MathUtil.applyDeadband(
-                                  -m_driverController.getRightY(), OIConstants.kDriveDeadband));
+                    updateInput();
                     m_robotDrive.drive(
                           leftInputVec,
                           rightInputVec,
@@ -61,6 +54,17 @@ public class RobotContainer {
             m_robotDrive));
 
     SmartDashboard.putData("Auto Chooser", autoChooser);
+  }
+
+  private void updateInput() {
+    leftInputVec.setX(MathUtil.applyDeadband(
+                              -m_driverController.getLeftY(), OIConstants.kDriveDeadband));
+                leftInputVec.setY(MathUtil.applyDeadband(
+                              -m_driverController.getLeftX(), OIConstants.kDriveDeadband));
+                rightInputVec.setX(MathUtil.applyDeadband(
+                              -m_driverController.getRightX(), OIConstants.kDriveDeadband));
+                rightInputVec.setY(MathUtil.applyDeadband(
+                              -m_driverController.getRightY(), OIConstants.kDriveDeadband));
   }
 
   // TODO: fill in placeholder commands with actual functionality
