00981fbfbc5efaed726a3d06f79162a82c74cc9e
Robert Sun <145725240+WashEyesWithTide@users.noreply.github.com>
2024-02-15 19:53:56 -0800

    Updated ShootSpeaker command
    Added Kinematics

--- src/main/java/frc/robot/commands/shooter/ShootSpeaker.java
+++ src/main/java/frc/robot/commands/shooter/ShootSpeaker.java
@@ -1,6 +1,8 @@
 package frc.robot.commands.shooter;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.constants.RobotConfig;
+import frc.robot.constants.RobotConstants;
 import frc.robot.subsystems.shooter.Shooter;
 
 public class ShootSpeaker extends Command {
@@ -16,9 +18,8 @@ public class ShootSpeaker extends Command {
   /**
    * Takes in distances to calculate shot, then shoots
    *
-   * Takes vertical height from constants
-   * Takes horizontal distance from vision
-   * Calculates angle and velocity
+   * <p>Takes vertical height from constants Takes horizontal distance from vision Calculates angle
+   * and velocity
    */
   // TODO: implement kinematics
   @Override
@@ -26,7 +27,12 @@ public class ShootSpeaker extends Command {
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    m_shooter.setAngle(calculateAngle(-1, RobotConfig.ShooterConfig.SpeakerHeight));
+    m_shooter.runFlywheel(
+        convertToRPM(calculateVelocity(-1, RobotConfig.ShooterConfig.SpeakerHeight)),
+        calculateVelocity(-1, RobotConfig.ShooterConfig.SpeakerHeight));
+  }
 
   // Called once the command ends or is interrupted.
   @Override
@@ -36,5 +42,60 @@ public class ShootSpeaker extends Command {
   @Override
   public boolean isFinished() {
     return false;
+  }
+
+  public double calculateAngle(double targetX, double targetY) {
+    /*if (targetX <= 0) {
+      end(true);
+    }*/
+    double time = Math.sqrt(2 * targetY / 9.8);
+    double xVelocity = targetX / time;
+    double yVelocity = 9.8 * time;
+    double theta = Math.atan(yVelocity / xVelocity);
+    return theta;
+  }
+
+  public double calculateVelocity(double targetX, double targetY) {
+    double time = Math.sqrt(2 * targetY / 9.8);
+    double xVelocity = targetX / time;
+    double yVelocity = 9.8 * time;
+    double velocity = Math.hypot(xVelocity, yVelocity);
+    return velocity;
+  }
+
+  public double calculateTotalDragForce(double velocity, double angle) {
+    // Convert angle to radians
+    double thetaRad = Math.toRadians(angle);
+    // trust
+    double cd = 0.25;
+    double rho = 1.869 * Math.pow(10, -5);
+    // help this is so wrong i dont even know
+    double area = 36 * 5 / 100;
+    // Resolve velocity vector into horizontal and vertical components
+    double vx = velocity * Math.cos(thetaRad);
+    double vy = velocity * Math.sin(thetaRad);
+    // Calculate drag force components
+    double dragForceX = 0.5 * cd * area * rho * vx * vx;
+    double dragForceY = 0.5 * cd * area * rho * vy * vy;
+    // Calculate total drag force
+    double totalDragForce = Math.hypot(dragForceX, dragForceY);
+    return (totalDragForce);
+  }
+
+  public double calculateFinalVelocity(double targetX, double targetY) {
+    double targetVelocity = calculateVelocity(targetX, targetY);
+    double theta = calculateAngle(targetX, targetY);
+    double finalVelocity = targetVelocity;
+    while (finalVelocity - calculateTotalDragForce(finalVelocity, theta) < targetVelocity - 0.25) {
+      finalVelocity = finalVelocity + calculateTotalDragForce(finalVelocity, theta);
+    }
+    return finalVelocity;
+  }
+
+  public double convertToRPM(double velocity) {
+    // 0.0762 is diameter of flywheel
+    double circumference = RobotConstants.ShooterConstants.FlywheelDiameter * Math.PI;
+    double rpm = velocity / circumference;
+    return rpm;
   }
 }
