2e3faaea320cd0ed960806348a0b18a2995723c9
Irishumanoid <95321751+Irishumanoid@users.noreply.github.com>
2024-02-17 21:02:11 -0800

    deploy to branch based on location overkill :D

--- deploy_stable.bat
+++ deploy_stable.bat
@@ -1,4 +1,20 @@
-set JAVA_HOME="C:\Users\Public\wpilib\2024\jdk"
-git switch main --force
+@echo on
+set JAVA_HOME="C:\Users\Public\wpilib\2024\jdk
+setlocal
+
+for /f "tokens=2 delims=:" %%a in ('"ipconfig | findstr IPv4"') do set ip=%%a
+set ip=%ip:~1%
+
+rem using ipinfo.io API to get region
+for /f "tokens=3 delims=\": %%i in ('curl -s ipinfo.io/%ip% ^| findstr /C:"city"') do set city=%%i
+
+if "%city%"=="Snohomish" (
+    git switch "Glacier_Peak_functional_branch" --force
+) else if "%city%"=="Bellevue" (
+    git switch "SAM_functional_branch" --force
+) else (
+    git switch main --force
+)
+
 call gradlew build
 pause
