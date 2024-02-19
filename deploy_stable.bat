2468195745b133c9751d88ed197863154d65cb66
Irishumanoid <95321751+Irishumanoid@users.noreply.github.com>
2024-02-19 14:40:13 -0800

    corrected syntax

--- deploy_stable.bat
+++ deploy_stable.bat
@@ -2,6 +2,12 @@
 set JAVA_HOME="C:\Users\Public\wpilib\2024\jdk
 setlocal
 
+ping -n 1 google.com > nul
+if %errorlevel% neq 0 (
+    echo Not connected to the internet, defaulting to deploying main branch.
+    goto :end
+)
+
 for /f "tokens=2 delims=:" %%a in ('"ipconfig | findstr IPv4"') do set ip=%%a
 set ip=%ip:~1%
 
@@ -9,12 +15,13 @@ rem using ipinfo.io API to get region
 for /f "tokens=3 delims=\": %%i in ('curl -s ipinfo.io/%ip% ^| findstr /C:"city"') do set city=%%i
 
 if "%city%"=="Snohomish" (
-    git switch "Glacier_Peak_functional_branch" --force
+    git switch --force "Glacier_Peak_functional_branch"
 ) else if "%city%"=="Bellevue" (
-    git switch "SAM_functional_branch" --force
+    git switch --force "SAM_functional_branch"
 ) else (
-    git switch main --force
+    :end
+    git switch --force main
 )
 
-call gradlew build
+call gradlew deploy
 pause
