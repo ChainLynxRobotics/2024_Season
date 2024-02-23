ef3b0c2a2cfcc106de6120a590691a9cee5e8196
ChainedGCH <failureeverywhere@gmail.com>
2024-02-23 14:07:51 -0800

    Added CAN motor ID

--- src/main/java/frc/robot/constants/RobotConstants.java
+++ src/main/java/frc/robot/constants/RobotConstants.java
@@ -126,9 +126,10 @@ public final class RobotConstants {
   public static final class IntakeConstants {
 
     // TODO: update placeholder digital input ID
+    // Pretend it doesn't exist
     public static final int kLineBreakSensor = -1;
 
     // TODO: update placeholder roller Motor ID
-    public static final int kMotorID = -1;
+    public static final int kMotorID = 10;
   }
 }
