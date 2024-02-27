4aa96d8936dc7bf736f82ef1f9a2fc9d12097263
iris <iris.litiu@gmail.com>
2024-02-26 19:12:51 -0800

    line break sensor reading

--- src/main/java/frc/robot/subsystems/intake/Intake.java
+++ src/main/java/frc/robot/subsystems/intake/Intake.java
@@ -2,20 +2,26 @@ package frc.robot.subsystems.intake;
 
 import com.revrobotics.CANSparkLowLevel.MotorType;
 import com.revrobotics.CANSparkMax;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.constants.RobotConstants.IntakeConstants;
 
 public class Intake extends SubsystemBase {
   private final CANSparkMax m_rollerMotor; // Intake roller motor
+  private final DigitalInput m_linebreak;
 
   // Constructs intake and initializes motor
   public Intake() {
     m_rollerMotor = new CANSparkMax(IntakeConstants.kMotorID, MotorType.kBrushless);
+    m_linebreak = new DigitalInput(IntakeConstants.kLineBreakSensor);
   }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    SmartDashboard.putBoolean("Intake/linebreak sensor", m_linebreak.get());
   }
 
   /**
