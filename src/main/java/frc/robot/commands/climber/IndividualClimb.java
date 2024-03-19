be98852b21db354ec515cfc3c84d7aea33070183
TurtleMeds <owenwells2009@gmail.com>
2024-03-19 16:51:07 -0700

    renamed variable for more clarity

--- src/main/java/frc/robot/commands/climber/IndividualClimb.java
+++ src/main/java/frc/robot/commands/climber/IndividualClimb.java
@@ -5,12 +5,12 @@ import frc.robot.subsystems.climber.Climber;
 
 public class IndividualClimb extends Command {
   private Climber m_climber;
-  private boolean m_isLeader;
+  private boolean m_isRight;
   private boolean m_reverse;
 
-  public IndividualClimb(Climber climber, boolean isLeader, boolean reverse) {
+  public IndividualClimb(Climber climber, boolean isRight, boolean reverse) {
     m_climber = climber;
-    m_isLeader = isLeader;
+    m_isRight = isRight;
     m_reverse = reverse;
 
     addRequirements(m_climber);
@@ -18,7 +18,7 @@ public class IndividualClimb extends Command {
 
   @Override
   public void initialize() {
-    if (m_isLeader) {
+    if (m_isRight) {
       m_climber.setLeader(m_reverse);
     } else {
       m_climber.setFollower(m_reverse);
@@ -27,7 +27,7 @@ public class IndividualClimb extends Command {
 
   @Override
   public void end(boolean interrupted) {
-    if (m_isLeader) {
+    if (m_isRight) {
       m_climber.stopLeader();
     } else {
       m_climber.stopFollower();
