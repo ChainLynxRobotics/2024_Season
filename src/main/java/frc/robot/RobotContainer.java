2d85d70b3b6965f55f3f02af9dc798c99535e10b
Irishumanoid <95321751+Irishumanoid@users.noreply.github.com>
2024-03-20 17:49:52 -0700

    only climber stuff for fewer merge conflits

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,91 +1,45 @@
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
-
 package frc.robot;
 
-import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
-import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.commands.climber.Climb;
 import frc.robot.commands.climber.IndividualClimb;
+import frc.robot.constants.RobotConstants.Bindings;
 import frc.robot.constants.RobotConstants.OIConstants;
 import frc.robot.subsystems.climber.Climber;
-import frc.robot.subsystems.drive.Drivetrain;
-import frc.utils.Vector;
 
 public class RobotContainer {
   private Joystick m_operatorController;
-  private XboxController m_driverController;
 
   private Climber m_climber;
-  private Drivetrain m_robotDrive;
-  private Vector leftInputVec;
-  private Vector rightInputVec;
 
   public RobotContainer() {
-    m_driverController = new XboxController(OIConstants.kDriverControllerPort);
     m_operatorController = new Joystick(OIConstants.kOperatorJoystickPort);
     m_climber = new Climber();
-    m_robotDrive = new Drivetrain();
-
-    leftInputVec = new Vector();
-    rightInputVec = new Vector();
-
-    m_robotDrive.setDefaultCommand(
-        // The left stick controls translation of the robot.
-        // Turning is controlled by the X axis of the right stick.
-        new RunCommand(
-            () -> {
-              // update the values of leftInputVec and rightInputVec to the values of the controller
-              // I'm avoiding re-instantiting Vectors to save memory
-              updateInput();
-              m_robotDrive.drive(
-                  leftInputVec,
-                  rightInputVec,
-                  m_driverController.getRightBumper(),
-                  m_driverController.getAButton());
-            },
-            m_robotDrive));
 
     configureBindings();
   }
 
   private void configureBindings() {
-    // new Trigger(() -> m_operatorController.getRawButton(15)).whileTrue(new Climb(m_climber,
-    // false));
 
-    // new Trigger(() -> m_operatorController.getRawButton(16)).whileTrue(new Climb(m_climber,
-    // true));
-
-    new Trigger(() -> m_operatorController.getRawButton(11))
+    new Trigger(() -> m_operatorController.getRawButton(Bindings.kRightClimberUp))
         .whileTrue(new IndividualClimb(m_climber, true, true));
-    new Trigger(() -> m_operatorController.getRawButton(12))
+    new Trigger(() -> m_operatorController.getRawButton(Bindings.kRightClimberDown))
         .whileTrue(new IndividualClimb(m_climber, true, false));
-    new Trigger(() -> m_operatorController.getRawButton(13))
+    new Trigger(() -> m_operatorController.getRawButton(Bindings.kLeftClimberUp))
         .whileTrue(new IndividualClimb(m_climber, false, true));
-    new Trigger(() -> m_operatorController.getRawButton(14))
+    new Trigger(() -> m_operatorController.getRawButton(Bindings.kLeftClimberDown))
         .whileTrue(new IndividualClimb(m_climber, false, false));
 
-    // up
-    new Trigger(() -> m_operatorController.getRawButton(15)).whileTrue(new Climb(m_climber, true));
-    // down
-    new Trigger(() -> m_operatorController.getRawButton(16)).whileTrue(new Climb(m_climber, false));
-  }
-
-  private void updateInput() {
-    leftInputVec.setX(
-        MathUtil.applyDeadband(-m_driverController.getLeftY(), OIConstants.kDriveDeadband));
-    leftInputVec.setY(
-        MathUtil.applyDeadband(-m_driverController.getLeftX(), OIConstants.kDriveDeadband));
-    rightInputVec.setX(
-        MathUtil.applyDeadband(-m_driverController.getRightX(), OIConstants.kDriveDeadband));
-    rightInputVec.setY(
-        MathUtil.applyDeadband(-m_driverController.getRightY(), OIConstants.kDriveDeadband));
+    new Trigger(() -> m_operatorController.getRawButton(Bindings.kBothClimbersUp))
+        .whileTrue(new Climb(m_climber, true));
+    new Trigger(() -> m_operatorController.getRawButton(Bindings.kBothClimbersDown))
+        .whileTrue(new Climb(m_climber, false));
   }
 
   public Command getAutonomousCommand() {
--- src/main/java/frc/robot/constants/RobotConfig.java
+++ src/main/java/frc/robot/constants/RobotConfig.java
@@ -1,13 +1,8 @@
 package frc.robot.constants;
 
-import com.pathplanner.lib.util.HolonomicPathFollowerConfig;
-import com.pathplanner.lib.util.PIDConstants;
-import com.pathplanner.lib.util.ReplanningConfig;
 import edu.wpi.first.units.Distance;
 import edu.wpi.first.units.Measure;
 import edu.wpi.first.units.Units;
-import frc.robot.constants.RobotConstants.DriveConstants;
-import frc.robot.constants.RobotConstants.SwerveModuleConstants;
 
 /**
  * Software config settings (e.g. max speed, PID values). For hardware constants @see
@@ -22,68 +17,5 @@ public class RobotConfig {
     public static final boolean kInverted = true;
     public static final Measure<Distance> buddyClimbExtensionDiff =
         Units.Meters.of(Units.Inches.of(5).in(Units.Meters));
-  }
-
-  public static class DriveConfig {
-    public static class TranslateConfig {
-      public static final String kPKey = "Vision Translate P";
-      public static final String kIKey = "Vision Translate I";
-      public static final String kDKey = "Vision Translate D";
-      public static final double kP = 0.0;
-      public static final double kI = 0.0;
-      public static final double kD = 0.0;
-      public static final double kTolerance = 1.0;
-      public static final double minIntegral = 0;
-      public static final double maxIntegral = 2;
-    }
-
-    public static class TurnConfig {
-      public static final String kPKey = "Vision Turn P";
-      public static final String kIKey = "Vision Turn I";
-      public static final String kDKey = "Vision Turn D";
-      public static final double kP = 0.0;
-      public static final double kI = 0.0;
-      public static final double kD = 0.0;
-      public static final double kTolerance = 1.0;
-      public static final double minIntegral = 0;
-      public static final double maxIntegral = 8;
-    }
-
-    public static final String kSlewRateTranslationMagOutput = "translation magnitude output";
-    public static final String kSlewRateTranslationDirRadOutput = "translation dir rad";
-
-    public static final HolonomicPathFollowerConfig kPathFollowerConfig =
-        new HolonomicPathFollowerConfig(
-            new PIDConstants(
-                SwerveModuleConstants.kDrivingP,
-                SwerveModuleConstants.kDrivingI,
-                SwerveModuleConstants.kDrivingD),
-            new PIDConstants(
-                SwerveModuleConstants.kTurningP,
-                SwerveModuleConstants.kTurningI,
-                SwerveModuleConstants.kTurningD),
-            SwerveModuleConstants.kMaxModuleSpeed,
-            DriveConstants.kWheelBaseRadius.in(Units.Meters),
-            new ReplanningConfig());
-
-    // 4.45 m/s max speed
-    public static final double kMaxSpeedBase = 4.8;
-    public static final double kMaxSpeedScaleFactor = 0.9;
-    public static final double kMaxSpeedMetersPerSecond = kMaxSpeedBase * kMaxSpeedScaleFactor;
-
-    public static final double kMaxAngularSpeedBase = Math.PI;
-    public static final double kMaxAngularSpeedScaleFactor = 0.7;
-    public static final double kMaxAngularSpeed =
-        kMaxAngularSpeedBase * kMaxAngularSpeedScaleFactor; // radians per second
-
-    public static final double kFrontLeftChassisAngularOffset = 0.0;
-    public static final double kFrontRightChassisAngularOffset = 0.0;
-    public static final double kBackLeftChassisAngularOffset = 0.0;
-    public static final double kBackRightChassisAngularOffset = 0.0;
-    // scaling factor for the alternative turning mode
-    public static final int altTurnSmoothing = 20;
-    public static final double HIGH_DIRECTION_SLEW_RATE = 500;
-    public static final double MIN_ANGLE_SLEW_RATE = 0.45 * Math.PI;
-    public static final double MAX_ANGLE_SLEW_RATE = 0.85 * Math.PI;
   }
 }
